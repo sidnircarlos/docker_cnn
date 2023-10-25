1a7f36039da2a5550eccca19364d9248a7410e8f
Sidnir Carlos <sidnircarlos@gmail.com>
2023-10-24 21:34:32 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Construa a imagem Docker
-docker build -t image-dl .
+docker build -t image-dl . &&
 
 # Execute o contêiner Docker
-docker run -d --rm --gpus all --name jupyterserver -p 8888:8888 -v "$PWD/notebooks:/home/notebooks" image-dl
+docker run -d --rm --gpus all --name jupyterserver -p 8888:8888 -v "$PWD/notebooks:/home/notebooks" image-dl &&
 
 # Exiba os contêineres em execução
-docker ps
+docker ps &&
 
 # Exiba os logs do contêiner
-docker container logs jupyterserver
+docker container logs jupyterserver 
