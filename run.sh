f94f4c4db8140978bfffc12db97e590f29cb2c30
Sidnir Carlos <sidnircarlos@gmail.com>
2023-10-24 21:30:34 -0300

    Adiciona o arquivo run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Construa a imagem Docker
+docker build -t image-dl .
+
+# Execute o contêiner Docker
+docker run -d --rm --gpus all --name jupyterserver -p 8888:8888 -v "$PWD/notebooks:/home/notebooks" image-dl
+
+# Exiba os contêineres em execução
+docker ps
+
+# Exiba os logs do contêiner
+docker container logs jupyterserver
